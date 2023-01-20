c533b9e4e8ecab94fc5f8db8eb6336f4e2f92d44
Cristian Muñoz <cristiand.munozt@outlook.com>
2023-01-20 18:21:50 -0500

    Semana 2

---
+++ src/semana2/valores.js
@@ -0,0 +1,18 @@
+typeof 44
+'number'
+typeof 44.67
+'number'
+typeof "Cristian"
+'string'
+typeof true
+'boolean'
+typeof false
+'boolean'
+typeof []
+'object'
+typeof {}
+'object'
+typeof null // Investiguen el porque de null es un objeto
+'object'
+typeof undefined
+'undefined'
